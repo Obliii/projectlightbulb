bc46f0b5ad6edcb7440fe5e1cb69444306a7c1bd
Obli <adrianmckay202@gmail.com>
2024-07-31 02:13:34 -0500

    File Cleanup and Adds music for testing
    Objects go into object folder, scenes are scenes, ui into a ui folder for clarity.
    
    Music made by Pedro Quintella has been added in. Added it to the Main Menu to test it out.

---
+++ objects/Player/player.gd
@@ -0,0 +1,27 @@
+class_name Player
+extends CharacterBody2D
+
+@export var sprite : AnimatedSprite2D
+@export var entity : CharacterBody2D
+
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+
+# Everything else is running on the PlayerStateMachine and its states.
+	
+func _physics_process(delta):
+	# TODO: Put Gravity into it's own state?
+	if !is_on_floor():
+		velocity.y += gravity * delta
+	
+	move_and_slide()
+
+func _on_area_2d_body_entered(body):
+	if body.is_in_group("Rigidbody"):
+		body.collision_layer = 1
+		body.collision_mask = 1
+
+
+func _on_area_2d_body_exited(body):
+	if body.is_in_group("Rigidbody"):
+		body.collision_layer = 2
+		body.collision_mask = 2
