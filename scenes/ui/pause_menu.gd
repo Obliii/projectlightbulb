36b5dfd3f3b8169d6fb23cd3fe6cab7009915849
cloudisalright <cloudisalright@gmail.com>
2024-07-31 15:32:31 -0500

    Pause Menu + Sound insert

---
+++ scenes/ui/pause_menu.gd
@@ -0,0 +1,40 @@
+extends Control
+
+@onready var main = $"../"
+@onready var pause_menu = $"."
+@onready var canvas_layer = $CanvasLayer
+
+var paused = false
+
+func _process(delta):
+	if Input.is_action_just_pressed("pause"):
+		pauseMenu()
+		print("pausing")
+	if Input.is_action_just_pressed("fullscreen_toggle"):
+		fullscreenToggle()
+		print("fullscreen toggled")
+
+func pauseMenu():
+	if paused:
+		pause_menu.hide()
+		canvas_layer.hide()
+		get_tree().paused = false
+	else:
+		get_tree().paused = true
+		canvas_layer.show()
+	
+	paused = !paused
+	
+func fullscreenToggle():
+	if  DisplayServer.window_get_mode() == 0:
+		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
+	elif DisplayServer.window_get_mode() == 3:
+		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
+		
+func _on_resume_pressed():
+	pauseMenu()
+	print("resuming")
+
+func _on_quit_pressed():
+	GameManager.QuitGame()
+	print("quiting")
