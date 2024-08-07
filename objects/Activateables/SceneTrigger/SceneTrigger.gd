ffdd17ce46fd25bf55eb0bea660776641a430f5c
Obli <adrianmckay202@gmail.com>
2024-08-06 20:34:03 -0500

    Redoes Scene Trigger
    Something broke. Readding Scene Trigger.

--- objects/Activateables/SceneTrigger/SceneTrigger.gd
+++ objects/Activateables/SceneTrigger/SceneTrigger.gd
@@ -1,5 +1,6 @@
 extends Area2D
 
+var player : Player
 @export var mode : TriggerModes = 0
 
 ## Multiple ways that Scene Trigger that could be used. Select a TriggerMode and only change the one that you selected.
@@ -20,11 +21,15 @@ extends Area2D
 ## How fast the camera will move when triggered.
 @export var TargetZoomSmoothening : float = 2.5
 ## Follow the player. That's it. Yep. 
-@export var TargetFollowPlayer : bool = false
+@export var TargetFollowPlayerX : bool = false
+@export var TargetFollowPlayerY : bool = false
 
 # Just simply changes the scene. I may add a fade to black, then loading screen.
 @export_subgroup("Change Scene")
 @export var NewScene : PackedScene
+
+@export_subgroup("Additional Settings")
+# SOON tm
 
 enum TriggerModes {
 	TRIGGER_ActivateTrigger,  # Player enters and something will activate. // FOR DOORS CLOSING BEHIND THE PLAYER TO KEEP THEM IN AREA - ETC.
@@ -36,6 +41,10 @@ func _ready():
 	pass
 
 func _on_body_entered(body):
+	
+	if player == null:
+		player = body
+	
 	if body.is_in_group("Player") and !triggered:
 		
 		if trigger_only_once:
@@ -51,11 +60,16 @@ func _on_body_entered(body):
 				return
 			
 			TriggerModes.TRIGGER_ChangeCamera:
-				if point != null:
+				var FollowingPlayer = (TargetFollowPlayerX or TargetFollowPlayerY)
+				
+				if (point != null and !FollowingPlayer) or FollowingPlayer:
 					# Change where the camera is going to go.
 					ChangeCamera()
 				else:
 					print("SCENE TRIGGER FAILED: Marker2D Required to show the camera where to go.")
+					
+				if point != null and FollowingPlayer:
+					print("SCENE TRIGGER WARNING: Remember that a Marker2D is not needed when you are simply following the player.")
 				return	
 				
 			TriggerModes.TRIGGER_ChangeScene:
@@ -75,10 +89,18 @@ func ActivateTrigger():
 			object.activate()
 
 func ChangeCamera():
-	if !TargetFollowPlayer:
-		GameManager.ChangeCameraPos.emit(point.global_position, TargetZoom, TargetFollowPlayer)
+	var FollowingPlayer = (TargetFollowPlayerX or TargetFollowPlayerY)
+	
+	if !FollowingPlayer:
+		if point != null:
+			GameManager.ChangeCameraPos.emit(point.global_position, TargetZoom, TargetFollowPlayerX, TargetFollowPlayerY)
+		
+	elif TargetFollowPlayerX and !TargetFollowPlayerY:
+		# Vector2 is zero because it is JUST following the player. It's going to reset immediately.
+		GameManager.ChangeCameraPos.emit(Vector2(player.global_position.x, player.global_position.y - 250), TargetZoom, TargetFollowPlayerX, TargetFollowPlayerY)
+			
 	else:
-		GameManager.ChangeCameraPos.emit(Vector2(0,0), TargetZoom, TargetFollowPlayer)
+		GameManager.ChangeCameraPos.emit(Vector2(player.global_position.x, player.global_position.y), TargetZoom, TargetFollowPlayerX, TargetFollowPlayerY)
 	
 func ChangeScene():
 	GameManager.ChangeLevelByScenePacked(NewScene)
