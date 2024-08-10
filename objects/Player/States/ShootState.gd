7fd77d73054be74d6aaa07b9a18bb3915a2e5dc4
Eisendrachen <firessscalesss@gmail.com>
2024-08-10 14:39:54 -0500

    Beam sound Effect + Double jump effect tweak
    It says on the title

--- objects/Player/States/ShootState.gd
+++ objects/Player/States/ShootState.gd
@@ -1,12 +1,14 @@
 class_name PlayerShootState
 extends PlayerState
 
+@onready var Pew = $"../../AudioStreamPlayer2D"
 var AnimationTime : float = 0.0
 var EndTime : float = 0.33
 
 func EnterState():
 	parent.sprite.play("StaffUse")
 	parent.fire_projectile()
+	Pew.playing = true
 
 func ExitState():
 	parent.velocity.x = 0
